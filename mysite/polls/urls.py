1e05c552909d4905f377eed7312aad1a9649dc4a
taoufik <taourirt@live.fr>
2021-05-04 15:27:24 +0200

    exo tuto partie 7

---
+++ mysite/polls/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 15:22:42 2021
+
+@author: TTaou
+"""
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
